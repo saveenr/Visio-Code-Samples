e387c038fe9c73df0e0b96502851576620ea2ab1
Saveen Reddy <saveenr@live.com>
2015-05-20 11:36:05 -0700

    Simplified the list template script

--- IronPython/ListTemplates.py
+++ IronPython/ListTemplates.py
@@ -16,35 +16,33 @@ ver_to_path = {
 def main() :
 
     versions = [ "2013" ]
-    
+
+    # Load Visio and prepare to use it
+    clr.AddReference("Microsoft.Office.Interop.Visio")
+    import Microsoft.Office.Interop.Visio
+    IVisio = Microsoft.Office.Interop.Visio    
+    flags= IVisio.VisOpenSaveArgs.visOpenRO | IVisio.VisOpenSaveArgs.visOpenDocked
+    visapp = IVisio.ApplicationClass()
+    docs = visapp.Documents
+    assert( 0 == docs.Count)
 
     for visio_version in versions:    
-        clr.AddReference("Microsoft.Office.Interop.Visio")
-        import Microsoft.Office.Interop.Visio
-        IVisio = Microsoft.Office.Interop.Visio
-    
-        print_masters = True
     
         stencil_path = System.IO.Path.Combine( ver_to_path[visio_version] , lang_to_id["en"] )
         vst_files = System.IO.Directory.GetFiles(stencil_path,"*.vst*")
-    
-        visapp = IVisio.ApplicationClass()
-        docs = visapp.Documents
-        flags= IVisio.VisOpenSaveArgs.visOpenRO | IVisio.VisOpenSaveArgs.visOpenDocked
-    
+           
         for vst_file in vst_files:
             
             doc = docs.Open( System.IO.Path.Combine( stencil_path, vst_file) )
-            n=0
+
             for d in docs :
-                if n>0:
-                    tokens  = [ ("Visio"+ visio_version) , doc.Name, doc.Title, d.Name, d.Title ]
-                    line = "|".join(tokens)
-                    print line
-                n+=1
+                tokens  = [ ("Visio"+ visio_version) , doc.Name, doc.Title, d.Name, d.Title ]
+                line = "|".join(tokens)
+                print line
+
             doc.Close()
         
-        # Once done, close visio
+    # Once done, close visio
     visapp.Quit()
     
 main()
