ebe8a4290f3bed0919e43d998b563a88382b09a2
Saveen Reddy <saveenr@live.com>
2015-05-20 05:34:06 -0700

    ListTemplates now works with vVisio 2013

--- IronPython/ListTemplates.py
+++ IronPython/ListTemplates.py
@@ -2,10 +2,7 @@ import sys
 import clr
 import System
 import os
-
-clr.AddReference("Microsoft.Office.Interop.Visio")
-import Microsoft.Office.Interop.Visio
-IVisio = Microsoft.Office.Interop.Visio
+import sys
 
 lang_to_id = {
               "en" : "1033"
@@ -13,34 +10,41 @@ lang_to_id = {
 
 ver_to_path = {
                "2007" : r"C:\Program Files (x86)\Microsoft Office\Office12",
-               "2010" : r"C:\Program Files (x86)\Microsoft Office\Office14\Visio Content"
-               }
+               "2010" : r"C:\Program Files (x86)\Microsoft Office\Office14\Visio Content",
+               "2013" : r"C:\Program Files\Microsoft Office 15\root\office15\Visio Content" }
 
 def main() :
-    visio_version = "2010"
-    print_masters = True
 
-    stencil_path = System.IO.Path.Combine( ver_to_path[visio_version] , lang_to_id["en"] )
-    vst_files = System.IO.Directory.GetFiles(stencil_path,"*.vst")
-
-    visapp = IVisio.ApplicationClass()
-    docs = visapp.Documents
-    flags= IVisio.VisOpenSaveArgs.visOpenRO | IVisio.VisOpenSaveArgs.visOpenDocked
-
-    for vst_file in vst_files:
-        
-        doc = docs.Open( System.IO.Path.Combine( stencil_path, vst_file) )
-        print
-        print "--------------"
-        print doc.Name, doc.Title
-        n=0
-        for d in docs :
-            if n>0:
-                print d.Name, d.Title
-            n+=1
-        doc.Close()
+    versions = [ "2013" ]
     
-    # Once done, close visio
+
+    for visio_version in versions:    
+        clr.AddReference("Microsoft.Office.Interop.Visio")
+        import Microsoft.Office.Interop.Visio
+        IVisio = Microsoft.Office.Interop.Visio
+    
+        print_masters = True
+    
+        stencil_path = System.IO.Path.Combine( ver_to_path[visio_version] , lang_to_id["en"] )
+        vst_files = System.IO.Directory.GetFiles(stencil_path,"*.vst*")
+    
+        visapp = IVisio.ApplicationClass()
+        docs = visapp.Documents
+        flags= IVisio.VisOpenSaveArgs.visOpenRO | IVisio.VisOpenSaveArgs.visOpenDocked
+    
+        for vst_file in vst_files:
+            
+            doc = docs.Open( System.IO.Path.Combine( stencil_path, vst_file) )
+            n=0
+            for d in docs :
+                if n>0:
+                    tokens  = [ ("Visio"+ visio_version) , doc.Name, doc.Title, d.Name, d.Title ]
+                    line = "|".join(tokens)
+                    print line
+                n+=1
+            doc.Close()
+        
+        # Once done, close visio
     visapp.Quit()
     
 main()
